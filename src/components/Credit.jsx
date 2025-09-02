6652bdc24ffb8a481c4c19bf10e7f99d7e36f4c7
akshprooo <akshprooo@gmail.com>
2025-09-02 18:49:13 +0530

    credit removed

--- src/components/Credit.jsx
+++ src/components/Credit.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Credit = () => {
   return (
-    <div className='fixed bottom-2 rounded-md text-white right-2 p-3 bg-amber-700 ' >
+    <div className='fixed bottom-2 hidden rounded-md text-white right-2 p-3 bg-amber-700 ' >
         <h2 className='font-semibold'>Website Made By <a href="http://akshprooo.vercel.app" className='underline font-bold'>Aksh Tiwari</a></h2>
     </div>
   )
